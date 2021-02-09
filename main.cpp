c2ba4ee594251c01e3248ea125aa946293289558
Hansi Gayathma <64369844+HansiGaya99@users.noreply.github.com>
2021-02-09 12:59:37 +0530

    File added

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*int main() {
+	cout << "Hello World!\n" <<endl;
+	return 0;
+}*/
+
+/*int main() {
+	string name;
+	cin >>name;
+	cout << name <<" Hello World!\n" <<endl;
+	return 0;
+}*/
+
+int main() {
+	string name;
+	cout <<"Please enter your name: ";
+	cin >>name;
+	cout <<"Hello " << name << endl;
+	return 0;
+}
